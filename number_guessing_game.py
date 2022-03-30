ffa18e2869be4446f6d17306d6fa68ca6b1cb872
tudorstb <93217943+tudorstb@users.noreply.github.com>
2022-03-31 00:57:22 +0300

    Number of attempt and list of it

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,5 +1,5 @@
 import random
-#i choose to implement a verification for the number in order to enter only intigers
+#i choose to implement a verification for the number in order to enter only intigers and numbers between a set numbers
 def verify_int(guess,start_nr,end_nr):
         while True:
             try:
@@ -20,11 +20,16 @@ def verify_int(guess,start_nr,end_nr):
 rand_number=random.randrange(1,20)
 start_nr = 1
 end_nr = 20
+list_of_attempt=[]
+number_of_attempt=0
 while True:
     guess=input(f"Guess an integer between {start_nr} and {end_nr}:")
     guess=verify_int(guess,start_nr,end_nr)
+    list_of_attempt.append(guess)
+    number_of_attempt+=1
     if guess==rand_number:
         print("You got it!")
+        print(f'You had {number_of_attempt} attempt :{list_of_attempt}')
         break
     elif guess>rand_number:
         print("The guess is too high!")
